9c6e9cad4a32f71b4bc0f10aabf64e5c35500c91
Stephen Geller <stephen.geller@hotmail.com>
2018-07-21 23:11:58 +0100

    spike(yearbook-game): Write possible MVP of lambda function & backend.

---
+++ src/QuotePicker.js
@@ -0,0 +1,12 @@
+class QuotePicker {
+    constructor(name, db) {
+        this.name = name.toLowerCase()
+        this.db = db
+    }
+
+    getQuote() {
+        this.db.findOne(this.name)
+    }
+}
+
+module.exports = QuotePicker;
