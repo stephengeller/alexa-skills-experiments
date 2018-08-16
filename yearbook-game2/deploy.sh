289ee52a6370b786dc5e2b748aad50a62162d25a
Stephen Geller <stephen.geller@192.168.19.128>
2018-08-16 14:43:48 +0100

    chore(deploy): Add flag to recursively add node_modules

--- yearbook-game2/deploy.sh
+++ yearbook-game2/deploy.sh
@@ -3,7 +3,7 @@
 set -e
 
 cd src
-zip ../yearbook_lambda_zipped *
+zip -r ../yearbook_lambda_zipped *
 cd -
 
-aws lambda update-function-code --region us-east-1 --function-name "yearbook-game" --zip-file fileb://yearbook_lambda_zipped.zip
+aws lambda update-function-code --publish --region us-east-1 --function-name "yearbook-game" --zip-file fileb://yearbook_lambda_zipped.zip
