1678579c4c7364acf6bb8297f3d7d9c81d06c20b
stephengeller <stephen.geller@hotmail.com>
2018-08-18 13:20:26 +0100

    feat(yearbook-game): Improve logic on response from Alexa

--- yearbook-game/deploy.sh
+++ yearbook-game/deploy.sh
@@ -2,12 +2,16 @@
 
 set -e
 
+cd src
+
+echo "Checking linting errors..."
+eslint index.js
 
 echo "Zipping up..."
-cd src
 zip -r ../yearbook_lambda_zipped * &>/dev/null
 cd - &>/dev/null
 
 echo "Uploading to AWS Lambda..."
 aws lambda update-function-code --publish --region us-east-1 --function-name "yearbook-game" --zip-file fileb://yearbook_lambda_zipped.zip &>/dev/null
+rm -rf yearbook_lambda_zipped.zip
 echo "Done."
--- yearbook-game/src/index.js
+++ yearbook-game/src/index.js
@@ -9,22 +9,35 @@ const TABLE_NAME = "yearbook-game-test-db"
 
 const handlers = {
 	LaunchRequest: function() {
+		this.attributes.sessionEnded = false
 		this.emitWithState("StartGame")
 	},
 	StartGame: function() {
-		this.attributes.someAttr = "value of some attribute " + Date()
-		this.response
-			.speak("Starting the Yearbook Game. Take a guess")
-			.listen("Listening")
-		this.emit(":responseReady")
+		const intro = "Starting the Yearbook Game. Who wrote this quote?"
+		getRandomQuoteFromDB(quote => {
+			this.attributes.quote = quote.quote
+			const quoteText = quote.quote.text
+			this.response
+				.speak(intro + " " + quoteText)
+				.listen("Either say the first name, or I don't know")
+			this.emit(":responseReady")
+		})
 	},
 	PlayAgain: function() {
 		this.response.speak("Let's go again! Take a guess").listen("Listening")
 		this.emit(":responseReady")
 	},
 	AnalyseResponse: function() {
-		const guess = this.event.request.intent.slots.guess.value
-		if (guess.length <= 5) {
+		const { slots } = this.event.request.intent
+		const guessNameOfAuthor = Object.keys(slots).includes("guess")
+			? slots.guess.value.toLowerCase()
+			: slots.first_name.value.toLowerCase()
+		const realNameOfAuthor = this.attributes.quote.from.toLowerCase()
+		const firstName = realNameOfAuthor.split(" ")[0]
+		if (
+			guessNameOfAuthor == firstName ||
+			guessNameOfAuthor.includes(firstName)
+		) {
 			// this.response.speak("Analysing Response").listen("Listening")
 			this.emitWithState("WinGame")
 		} else {
@@ -32,11 +45,17 @@ const handlers = {
 		}
 	},
 	WinGame: function() {
-		this.response.speak("You won. Play again?").listen("Play again?")
+		const correctAnswer = this.attributes.quote.from
+		this.response
+			.speak("You won! It was: " + correctAnswer + ". Play again?")
+			.listen("Play again?")
 		this.emit(":responseReady")
 	},
 	LoseGame: function() {
-		this.response.speak("You lose. Play again?").listen("Play again?")
+		const correctAnswer = this.attributes.quote.from
+		this.response
+			.speak("You lost! It was: " + correctAnswer + ". Play again?")
+			.listen("Play again?")
 		this.emit(":responseReady")
 	},
 	"AMAZON.StopIntent": function() {
@@ -57,41 +76,25 @@ const handlers = {
 		this.attributes.sessionEnded = true
 		// console.log("session ended!")
 		this.emit(":saveState", true)
-	},
-	GetFromDynamoDb: async function() {
-		console.log("getting from db")
-		const params = {
-			TableName: TABLE_NAME
-		}
-
-		const putParams = {
-			TableName: TABLE_NAME,
-			Item: {
-				quote: {
-					text: `some_quote_${Date.now()}`,
-					from: "some_author",
-					to: "some_person"
-				},
-				userId: String(Date.now())
-			}
-		}
-
-		await docClient.scan(params, (err, data) => {
-			if (err) {
-				this.response.speak("Quote error.")
-				this.emit(":responseReady")
-			} else {
-				const { Items } = data
-				const quotes = Items.filter(item => {
-					return Object.keys(item).includes("quote")
-				})
-				this.response.speak(
-					`Quote found: ${quotes[quotes.length - 1].quote.text}`
-				)
-				this.emit(":responseReady")
-			}
-		})
 	}
+}
+
+const getRandomQuoteFromDB = async callback => {
+	const params = { TableName: TABLE_NAME }
+
+	await docClient.scan(params, (err, data) => {
+		if (err) {
+			return null
+		} else {
+			const { Items } = data
+			const quotes = Items.filter(item => {
+				return Object.keys(item).includes("quote")
+			})
+			const randomQuote = quotes[Math.floor(Math.random() * quotes.length)]
+			console.log("Sending quote: " + randomQuote.userId)
+			callback(randomQuote)
+		}
+	})
 }
 
 exports.handler = function(event, context, callback) {
