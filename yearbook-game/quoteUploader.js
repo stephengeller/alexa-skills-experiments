375cb022cf190ed860860f04ebf6a7a5832fcb4e
Stephen Geller <stephen.gb.geller+github@gmail.com>
2018-12-06 10:44:11 +0000

    refactor(linting): Clean up code.

--- yearbook-game/quoteUploader.js
+++ yearbook-game/quoteUploader.js
@@ -1,20 +1,20 @@
-const AWS = require("aws-sdk")
-const docClient = new AWS.DynamoDB.DocumentClient({ region: "us-east-1" })
-const data = require("./quotes.json")
+const AWS = require("aws-sdk");
+const docClient = new AWS.DynamoDB.DocumentClient({ region: "us-east-1" });
+const data = require("./quotes.json");
 
 const writeToDB = async params => {
-	const { from, to } = params.Item.quote
-	console.log(`Uploading quote from ${from} to ${to}`)
+	const { from, to } = params.Item.quote;
+	console.log(`Uploading quote from ${from} to ${to}`);
 
 	await docClient.put(params, async (err, data) => {
 		if (err) {
-			console.log("Writing error")
+			console.log("Writing error");
 			console.log(err)
 		} else {
 			await console.log("Upload successful\n")
 		}
 	})
-}
+};
 
 async function uploadQuotes(quotes) {
 	for (let person of quotes.data) {
@@ -28,17 +28,17 @@ async function uploadQuotes(quotes) {
 				},
 				userId: ""
 			}
-		}
+		};
 		for (let quote of person.quotes) {
-			let { from, text } = quote
-			console.log(from, person.to)
-			putParams.Item.quote.from = from
-			putParams.Item.quote.text = text
+			let { from, text } = quote;
+			console.log(from, person.to);
+			putParams.Item.quote.from = from;
+			putParams.Item.quote.text = text;
 			putParams.Item.userId = `from_${from
 				.toLowerCase()
-				.replace(" ", "_")}_to_${person.to.toLowerCase().replace(" ", "_")}`
+				.replace(" ", "_")}_to_${person.to.toLowerCase().replace(" ", "_")}`;
 			await writeToDB(putParams)
 		}
 	}
 }
-uploadQuotes(data)
+uploadQuotes(data);
--- yearbook-game/src/index.js
+++ yearbook-game/src/index.js
@@ -1,39 +1,39 @@
-"use strict"
+"use strict";
 
-const Alexa = require("alexa-sdk")
-const AWS = require("aws-sdk")
-const docClient = new AWS.DynamoDB.DocumentClient({ region: "us-east-1" })
+const Alexa = require("alexa-sdk");
+const AWS = require("aws-sdk");
+const docClient = new AWS.DynamoDB.DocumentClient({ region: "us-east-1" });
 
-const APP_ID = "ask/dadd2241-57ee-49ef-8b6b-bccb70fc2d59"
-const TABLE_NAME = "yearbook-game-test-db"
+const APP_ID = "ask/dadd2241-57ee-49ef-8b6b-bccb70fc2d59";
+const TABLE_NAME = "yearbook-game-test-db";
 
 const handlers = {
 	LaunchRequest: function() {
-		this.attributes.sessionEnded = false
+		this.attributes.sessionEnded = false;
 		this.emitWithState("StartGame")
 	},
 	StartGame: function() {
-		const intro = "Starting the Yearbook Game. Who wrote this quote?"
+		const intro = "Starting the Yearbook Game. Who wrote this quote?";
 		getRandomQuoteFromDB(quote => {
-			this.attributes.quote = quote.quote
-			const quoteText = quote.quote.text
+			this.attributes.quote = quote.quote;
+			const quoteText = quote.quote.text;
 			this.response
 				.speak(intro + " " + quoteText)
-				.listen("Either say the first name, or I don't know")
+				.listen("Either say the first name, or I don't know");
 			this.emit(":responseReady")
 		})
 	},
 	PlayAgain: function() {
-		this.response.speak("Let's go again! Take a guess").listen("Listening")
+		this.response.speak("Let's go again! Take a guess").listen("Listening");
 		this.emit(":responseReady")
 	},
 	AnalyseResponse: function() {
-		const { slots } = this.event.request.intent
+		const { slots } = this.event.request.intent;
 		const guessNameOfAuthor = Object.keys(slots).includes("guess")
 			? slots.guess.value.toLowerCase()
-			: slots.first_name.value.toLowerCase()
-		const realNameOfAuthor = this.attributes.quote.from.toLowerCase()
-		const firstName = realNameOfAuthor.split(" ")[0]
+			: slots.first_name.value.toLowerCase();
+		const realNameOfAuthor = this.attributes.quote.from.toLowerCase();
+		const firstName = realNameOfAuthor.split(" ")[0];
 		if (
 			guessNameOfAuthor == firstName ||
 			guessNameOfAuthor.includes(firstName)
@@ -45,21 +45,21 @@ const handlers = {
 		}
 	},
 	WinGame: function() {
-		const correctAnswer = this.attributes.quote.from
+		const correctAnswer = this.attributes.quote.from;
 		this.response
 			.speak("You won! It was: " + correctAnswer + ". Play again?")
-			.listen("Play again?")
+			.listen("Play again?");
 		this.emit(":responseReady")
 	},
 	LoseGame: function() {
-		const correctAnswer = this.attributes.quote.from
+		const correctAnswer = this.attributes.quote.from;
 		this.response
 			.speak("You lost! It was: " + correctAnswer + ". Play again?")
-			.listen("Play again?")
+			.listen("Play again?");
 		this.emit(":responseReady")
 	},
 	"AMAZON.StopIntent": function() {
-		this.response.speak("Ok, let's play again soon.")
+		this.response.speak("Ok, let's play again soon.");
 		this.emit(":responseReady")
 	},
     "AMAZON.HelpIntent": function() {
@@ -69,42 +69,42 @@ const handlers = {
 
 	// Cancel
 	"AMAZON.CancelIntent": function() {
-		this.response.speak("Ok, let's play again soon.")
+		this.response.speak("Ok, let's play again soon.");
 		this.emit(":responseReady")
 	},
 	"AMAZON.FallbackIntent": function() {
-		this.response.speak("Falling back, something went wrong.")
+		this.response.speak("Falling back, something went wrong.");
 		this.emit(":responseReady")
 	},
 	SessionEndedRequest: function() {
-		this.attributes.sessionEnded = true
+		this.attributes.sessionEnded = true;
 		// console.log("session ended!")
 		this.emit(":saveState", true)
 	}
-}
+};
 
 const getRandomQuoteFromDB = async callback => {
-	const params = { TableName: TABLE_NAME }
+	const params = { TableName: TABLE_NAME };
 
 	await docClient.scan(params, (err, data) => {
 		if (err) {
 			return null
 		} else {
-			const { Items } = data
+			const { Items } = data;
 			const quotes = Items.filter(item => {
 				return Object.keys(item).includes("quote")
-			})
-			const randomQuote = quotes[Math.floor(Math.random() * quotes.length)]
-			console.log("Sending quote: " + randomQuote.userId)
+			});
+			const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+			console.log("Sending quote: " + randomQuote.userId);
 			callback(randomQuote)
 		}
 	})
-}
+};
 
 exports.handler = function(event, context, callback) {
-	const alexa = Alexa.handler(event, context, callback)
-	alexa.dynamoDBTableName = TABLE_NAME
-	alexa.APP_ID = APP_ID
-	alexa.registerHandlers(handlers)
+	const alexa = Alexa.handler(event, context, callback);
+	alexa.dynamoDBTableName = TABLE_NAME;
+	alexa.APP_ID = APP_ID;
+	alexa.registerHandlers(handlers);
 	alexa.execute()
-}
+};
